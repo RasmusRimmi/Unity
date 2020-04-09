64094cfbb4caa99f3fe0b92194869ae6b1e82299
RasmusRimmi <rasmus136@gmail.com>
2020-04-09 16:24:35 +0300

    Tehtäviä

---
+++ Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float delay = .5f;
+    private float nextSpawn = -.5f;
+        
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time - nextSpawn > delay)
+        {
+            nextSpawn = Time.time;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
